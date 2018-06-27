c20f294f518f9d9df81da06952a1acad3085b515
Louis Sicard <lsicard@core-techs.fr>
2018-06-27 17:47:46 +0200

    Improving request listener

---
+++ src/Routing/AzureAuthLoader.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LouisSicard\AzureAuth\Routing;
+
+use Symfony\Component\Config\Loader\Loader;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+class AzureAuthLoader extends Loader
+{
+  private $isLoaded = false;
+
+  public function load($resource, $type = null)
+  {
+    if(true === $this->isLoaded) {
+      throw new \RuntimeException('Do not add the "azure_auth" loader twice');
+    }
+
+    $routes = new RouteCollection();
+
+    $routes->add(
+      'azure_auth_redirect',
+      new Route(
+        '/azure_auth_redirect',
+        array(
+          '_controller' => 'LouisSicard\AzureAuth\Controller\AzureAuthController::redirect',
+        ),
+        [])
+    );
+
+    $routes->add(
+      'azure_auth_logout',
+      new Route(
+        '/azure_auth_logout',
+        array(
+          '_controller' => 'LouisSicard\AzureAuth\Controller\AzureAuthController::logout',
+        ),
+        [])
+    );
+
+    $this->isLoaded = true;
+
+    return $routes;
+  }
+
+  public function supports($resource, $type = null)
+  {
+    return 'azure_auth' === $type;
+  }
+
+}
