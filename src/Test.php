58d37338572912211fcea91d94808c1fc2b9fac0
Louis Sicard <lsicard@core-techs.fr>
2018-06-27 11:30:43 +0200

    Initial commit

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: louis
+ * Date: 27/06/2018
+ * Time: 11:12
+ */
+
+namespace Adimeo\AzureAuth;
+
+
+class Test
+{
+
+  public function __construct() {
+
+  }
+
+}
