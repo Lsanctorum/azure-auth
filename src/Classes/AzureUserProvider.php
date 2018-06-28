8ecc92f64d7705407b8ac7f277f4b53907418960
Louis Sicard <lsicard@core-techs.fr>
2018-06-28 15:29:37 +0200

    Improving request listener

--- src/Classes/AzureUserProvider.php
+++ src/Classes/AzureUserProvider.php
@@ -7,7 +7,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 interface AzureUserProvider extends UserProviderInterface
 {
-
+  function getSupportedClass();
   function persist(UserInterface $user);
 
 }
